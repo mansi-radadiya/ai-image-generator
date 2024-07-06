c03c92fa351a4392b895d73928d0b8f70a67f961
Bhargav-Tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-07-07 01:37:31 +0530

    fixed height issue and addes some style change and animations

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,4 +1,4 @@
-import React ,{useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import './ImageGenerator.css';
 import img1 from '../Assets/default_image.jpg';
 
@@ -6,10 +6,10 @@ function ImageGenerator() {
 
   const [imageUrl, setImageUrl] = useState("/");
   let inputRef = useRef(null);
-  const [loading,setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const imageGenerator = async () => {
-    if (inputRef.current.value === "") {
+    if (inputRef?.current?.value === "") {
       return 0;
     }
 
@@ -17,17 +17,17 @@ function ImageGenerator() {
 
     const response = await fetch("https://api.openai.com/v1/images/generations",
       {
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
           Authorization:
-          "Bearer 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",
-          "User-Agent":"Chrome",
+            "Bearer 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",
+          "User-Agent": "Chrome",
         },
-        body:JSON.stringify({ 
-          prompt:`${inputRef.current.value}`,
-          n:1,
-          size:"512x512",
+        body: JSON.stringify({
+          prompt: `${inputRef?.current?.value}`,
+          n: 1,
+          size: "512x512",
         }),
       }
     );
@@ -43,28 +43,28 @@ function ImageGenerator() {
     console.log(data);
 
     let data_array = data.data;
-    setImageUrl(data_array[0].url);
-    setLoading(flase);
+    setImageUrl(data_array[0]?.url);
+    setLoading(false);
   }
 
 
   return (
     <div className='container'>
-        <div className='header'>AI Image <span>Generator</span></div>
-        <div className='header-1'>Paint a picture with your words of something you'd love to experience.</div>
-        <div className="imageLoading">
-          <div className="image">
-            <img src={imageUrl === "/" ? img1:imageUrl} alt="img" />
-          </div>
-          <div className="loading">
-            <div className={loading?"loadingBarFull" : "loadingBar"}></div>
-            <div className={loading?"loadingText" : "displayNone"}>Loading...</div>
-          </div>
+      <div className='header'>AI Image <span>Generator</span></div>
+      <div className='header-1'>Paint a picture with your words of something you'd love to experience.</div>
+      <div className="imageLoading">
+        <div className="image">
+          <img src={imageUrl === "/" ? img1 : imageUrl} alt="img" />
         </div>
-        <div className='searchBox'>
-          <input type="text" ref={inputRef} className='searchInput' placeholder='Describe What You Want To See'/>
-          <button className="generateButton" onClick={imageGenerator}>Generate</button>
+        <div className="loading">
+          <div className={loading ? "loadingBarFull" : "loadingBar"}></div>
+          <div className={loading ? "loadingText" : "displayNone"}>Loading...</div>
         </div>
+      </div>
+      <div className='searchBox'>
+        <input type="text" ref={inputRef} className='searchInput' placeholder='Describe What You Want To See' />
+        <button className="generateButton" onClick={imageGenerator}>Generate</button>
+      </div>
     </div>
   )
 }
